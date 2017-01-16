ffb79c8a071af6a4beefc6c869b17eddc781fb81
peterlee <pierre0218@gmail.com>
2017-01-16 23:15:14 +0800

    first commit

---
+++ Assets/PocketSphinxDemo/Source/SpeechRecognizerDemo.cs
@@ -0,0 +1,333 @@
+﻿//#define USE_CHINESE
+#define USE_ENGLISH
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class SpeechRecognizerDemo : MonoBehaviour, IPocketSphinxEvents
+{
+	public static bool UseChinese = false;
+
+    /* Named searches allow to quickly reconfigure the decoder */
+#if USE_CHINESE
+    private const String KWS_SEARCH = "action";
+#else
+	private const String KWS_SEARCH = "wakeup";
+#endif
+    private const String ACTION_SEARCH = "action";
+
+
+    /* Keyword we are looking for to activate menu */
+    private const String KEYPHRASE = "peanut listen";
+
+    #region Public serialized fields
+	[SerializeField]
+	private DogControl _dogControl;
+    [SerializeField]
+    private GameObject _pocketSphinxPrefab;
+    [SerializeField]
+    private Text _infoText;
+    [SerializeField]
+    private Text _SpeechResult;
+    [SerializeField]
+    private string[] progressTexts;
+    #endregion
+
+    #region Private fields
+    private UnityPocketSphinx.PocketSphinx _pocketSphinx;
+
+    private Dictionary<string, string> infoTextDict;
+    #endregion
+
+    #region Private methods
+    private void SubscribeToPocketSphinxEvents()
+    {
+        EM_UnityPocketsphinx em = _pocketSphinx.EventManager;
+
+        em.OnBeginningOfSpeech += OnBeginningOfSpeech;
+        em.OnEndOfSpeech += OnEndOfSpeech;
+        em.OnError += OnError;
+        em.OnInitializeFailed += OnInitializeFailed;
+        em.OnInitializeSuccess += OnInitializeSuccess;
+        em.OnPartialResult += OnPartialResult;
+        em.OnPocketSphinxError += OnPocketSphinxError;
+        em.OnResult += OnResult;
+        em.OnTimeout += OnTimeout;
+    }
+
+    private void UnsubscribeFromPocketSphinxEvents()
+    {
+        EM_UnityPocketsphinx em = _pocketSphinx.EventManager;
+
+        em.OnBeginningOfSpeech -= OnBeginningOfSpeech;
+        em.OnEndOfSpeech -= OnEndOfSpeech;
+        em.OnError -= OnError;
+        em.OnInitializeFailed -= OnInitializeFailed;
+        em.OnInitializeSuccess -= OnInitializeSuccess;
+        em.OnPartialResult -= OnPartialResult;
+        em.OnPocketSphinxError -= OnPocketSphinxError;
+        em.OnResult -= OnResult;
+        em.OnTimeout -= OnTimeout;
+    }
+
+    private void switchSearch(string searchKey)
+    {
+        _pocketSphinx.StopRecognizer();
+
+        if (searchKey.Equals(KWS_SEARCH))
+        {
+            _pocketSphinx.StartListening(searchKey);
+        }
+        else
+        {
+            _pocketSphinx.StartListening(searchKey,3000);
+        }
+
+        string text;
+        infoTextDict.TryGetValue(searchKey, out text);
+
+        _infoText.text = text;
+        _SpeechResult.text = "Say something!";
+    }
+    #endregion
+
+    #region MonoBehaviour methods
+    void Awake()
+    {
+        UnityEngine.Assertions.Assert.IsNotNull(_pocketSphinxPrefab, "No PocketSphinx prefab assigned.");
+        var obj = Instantiate(_pocketSphinxPrefab, this.transform) as GameObject;
+        _pocketSphinx = obj.GetComponent<UnityPocketSphinx.PocketSphinx>();
+
+        if (_pocketSphinx == null)
+        {
+            Debug.LogError("[SpeechRecognizerDemo] No PocketSphinx component found. Did you assign the right prefab???");
+        }
+
+        SubscribeToPocketSphinxEvents();
+
+        _infoText.text = "Please wait for Speech Recognition engine to load.";
+        _SpeechResult.text = "Loading human dictionary...";
+    }
+
+    void Start()
+    {
+#if USE_CHINESE
+		_pocketSphinx.SetAcousticModelPath("zh_broadcastnews_ptm256_8000");
+		_pocketSphinx.SetDictionaryPath("zh_broadcastnews_utf8.dict");
+#else
+		_pocketSphinx.SetAcousticModelPath("en-us-ptm");
+		_pocketSphinx.SetDictionaryPath("cmudict-en-us.dict");
+#endif
+        //Debug.Log("[SpeechRecognizerDemo] " + Application.streamingAssetsPath + "cmudict-en-us.dict");
+
+        _pocketSphinx.SetKeywordThreshold(1e-45f);
+        _pocketSphinx.AddBoolean("-allphone_ci", true);
+
+
+        _pocketSphinx.AddNGramSearchPath(ACTION_SEARCH, "commands.lm");
+
+        _pocketSphinx.SetupRecognizer();
+
+        infoTextDict = new Dictionary<string, string>();
+		#if USE_CHINESE
+		infoTextDict.Add(KWS_SEARCH, "請下指令");
+		#else
+        infoTextDict.Add(KWS_SEARCH, progressTexts[0]);
+		#endif
+		infoTextDict.Add(ACTION_SEARCH, progressTexts[1]);
+
+    }
+    
+    // Update is called once per frame
+    void Update ()
+    {
+        
+    }
+
+    void OnDestroy()
+    {
+        if (_pocketSphinx != null)
+        {
+            UnsubscribeFromPocketSphinxEvents();
+            _pocketSphinx.DestroyRecognizer();
+        }
+    }
+    #endregion
+
+    #region PocketSphinx event methods
+    public void OnPartialResult(string hypothesis)
+    {
+        _SpeechResult.text = hypothesis;
+
+		string[] tokens = hypothesis.Split(' ');
+		string lastword = "";
+		string lastword2 = "";
+		if (tokens.Length > 0) {
+			lastword = tokens [tokens.Length - 1];
+		}
+		if (tokens.Length > 1) {
+			lastword2 = tokens [tokens.Length - 2];
+		}
+
+#if USE_CHINESE
+		if (lastword2 == "站" && lastword == "好") {
+			_dogControl.SetTransition (Transition.ToStand);
+			switchSearch (KWS_SEARCH);
+		}
+
+		if (lastword2 == "装" && lastword == "死") {
+			_dogControl.SetTransition (Transition.ToDead);
+			switchSearch (KWS_SEARCH);
+		}
+
+		if (lastword2 == "跑" && lastword == "步") {
+			_dogControl.SetTransition (Transition.ToRun);
+			switchSearch (KWS_SEARCH);
+		}
+
+		if (lastword2 == "坐" && lastword == "下") {
+			_dogControl.SetTransition (Transition.ToSit);
+			switchSearch (KWS_SEARCH);
+		}
+
+		if (lastword2 == "趴" && lastword == "下") {
+			_dogControl.SetTransition (Transition.ToLay);
+			switchSearch (KWS_SEARCH);
+		}
+
+		if(lastword == "跳")
+		{
+			_dogControl.SetTransition (Transition.ToJump);
+			switchSearch (KWS_SEARCH);
+		}
+
+		if(lastword == "坐")
+		{
+			_dogControl.SetTransition (Transition.ToSit);
+			switchSearch (KWS_SEARCH);
+		}
+
+		if(lastword == "跑")
+		{
+			_dogControl.SetTransition (Transition.ToRun);
+			switchSearch (KWS_SEARCH);
+		}
+			
+#else
+		if (lastword == "down") {
+			if (lastword2 == "sit") {
+				_dogControl.SetTransition (Transition.ToSit);
+				switchSearch (KWS_SEARCH);
+			}
+			if (lastword2 == "lay") {
+				_dogControl.SetTransition (Transition.ToLay);
+				switchSearch (KWS_SEARCH);
+			}
+		}
+
+		if (lastword2 == "stand" && lastword == "up") {
+			_dogControl.SetTransition (Transition.ToStand);
+			switchSearch (KWS_SEARCH);
+		}
+
+		if (lastword2 == "play" && lastword == "dead") {
+			_dogControl.SetTransition (Transition.ToDead);
+			switchSearch (KWS_SEARCH);
+		}
+
+		if (hypothesis.Equals (KEYPHRASE))
+		{
+			switchSearch (ACTION_SEARCH);
+			_dogControl.SetTransition (Transition.ToStand);
+		}
+		else if(lastword.Equals ("walk"))
+		{
+			_dogControl.SetTransition (Transition.ToWalk);
+			switchSearch (KWS_SEARCH);
+		}
+		else if(lastword.Equals ("run"))
+		{
+			_dogControl.SetTransition (Transition.ToRun);
+			switchSearch (KWS_SEARCH);
+		}
+		else if(lastword.Equals ("stand"))
+		{
+			_dogControl.SetTransition (Transition.ToStand);
+			switchSearch (KWS_SEARCH);
+		}
+		else if(lastword.Equals ("sit"))
+		{
+			_dogControl.SetTransition (Transition.ToSit);
+			switchSearch (KWS_SEARCH);
+		}
+		else if(lastword.Equals ("lay"))
+		{
+			_dogControl.SetTransition (Transition.ToLay);
+			switchSearch (KWS_SEARCH);
+		}
+		else if(lastword.Equals ("jump"))
+		{
+			_dogControl.SetTransition (Transition.ToJump);
+			switchSearch (KWS_SEARCH);
+		}
+#endif
+    }
+
+    public void OnResult(string hypothesis)
+    {
+        _SpeechResult.text = hypothesis;
+    }
+
+    public void OnBeginningOfSpeech()
+    {
+        
+    }
+
+    public void OnEndOfSpeech()
+    {
+       // switchSearch(KWS_SEARCH);
+		_SpeechResult.text = "";
+    }
+
+    public void OnError(string error)
+    {
+        Debug.LogError("[SpeechRecognizerDemo] An error ocurred at OnError()");
+        Debug.LogError("[SpeechRecognizerDemo] error = " + error);
+
+		_SpeechResult.text = error;
+    }
+
+    public void OnTimeout()
+    {
+        Debug.Log("[SpeechRecognizerDemo] Speech Recognition timed out");
+        switchSearch(KWS_SEARCH);
+    }
+
+    public void OnInitializeSuccess()
+    {
+#if USE_ENGLISH
+        _pocketSphinx.AddKeyphraseSearch(KWS_SEARCH, KEYPHRASE);
+#endif
+		switchSearch(KWS_SEARCH);
+    }
+
+    public void OnInitializeFailed(string error)
+    {
+        Debug.LogError("[SpeechRecognizerDemo] An error ocurred on Initialization PocketSphinx.");
+        Debug.LogError("[SpeechRecognizerDemo] error = " + error);
+
+		_SpeechResult.text = error;
+    }
+
+    public void OnPocketSphinxError(string error)
+    {
+        Debug.LogError("[SpeechRecognizerDemo] An error ocurred on OnPocketSphinxError().");
+        Debug.LogError("[SpeechRecognizerDemo] error = " + error);
+
+		_SpeechResult.text = error;
+    }
+    #endregion
+}
---
+++ Assets/Scripts/DogControl.cs
@@ -0,0 +1,527 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public class DogControl : MonoBehaviour
+{
+	public Animator anim;
+	public string AnimatorName;
+	public int Move;
+
+	public int Pose = 0;
+	public int CurrentPose = 0;
+	public bool ChangePose = false;
+
+	public float CrossfadeVal = 0.25f;
+
+	private FSMSystem fsm;
+
+	public void SetTransition(Transition t) { fsm.PerformTransition(t); }
+
+	public void Start()
+	{
+		anim = GetComponent<Animator> ();
+		AnimatorName = anim.name;
+
+		MakeFSM();
+
+		SetTransition (Transition.ToRun);
+	}
+
+	public void Update()
+	{
+		if (ChangePose) 
+		{
+			print ("Change Pose");
+			ChangePose = false;
+			//if stands
+			if (CurrentPose == 0) {
+                if (Pose == 2) {
+					anim.CrossFade (AnimatorName +"IdleToSit", CrossfadeVal);
+				} else if (Pose == 3) {
+					anim.CrossFade (AnimatorName + "IdleToLay", CrossfadeVal);
+				} 
+
+				CurrentPose = Pose;
+			}
+			//Jump
+			else if (CurrentPose == 1) {
+				if (Pose == 2) {
+					anim.CrossFade (AnimatorName +"IdleToSit", CrossfadeVal);
+				} else if (Pose == 3) {
+					anim.CrossFade (AnimatorName + "IdleToLay", CrossfadeVal);
+				} 
+
+				CurrentPose = Pose;
+			}
+			//Sit
+			else if (CurrentPose == 2) {
+				if (Pose == 0) {
+					anim.CrossFade (AnimatorName + "SitToIdle", CrossfadeVal);
+				} else if (Pose == 3) {
+					anim.CrossFade (AnimatorName + "SitToLay", CrossfadeVal);
+				}
+
+				CurrentPose = Pose;
+			}
+			//Lay
+			else if (CurrentPose == 3) {
+				if (Pose == 0) {
+					anim.CrossFade (AnimatorName + "LayToIdle", CrossfadeVal);
+				}else if (Pose == 2) {
+					anim.CrossFade (AnimatorName + "LayToSit", CrossfadeVal);
+				}
+				CurrentPose = Pose;
+			}
+			//walk
+			else if (CurrentPose == 4) {
+				if (Pose == 0) {
+					anim.CrossFade (AnimatorName + "Idle", CrossfadeVal);
+				} else if (Pose == 2) {
+					anim.CrossFade (AnimatorName + "IdleToSit", CrossfadeVal);
+				} else if (Pose == 3) {
+					anim.CrossFade (AnimatorName + "IdleToLay", CrossfadeVal);
+				}
+
+				CurrentPose = Pose;
+			} 
+			//Run
+			else if (CurrentPose == 5) {
+				if (Pose == 0) {
+					anim.CrossFade (AnimatorName + "Idle", CrossfadeVal);
+				} else if (Pose == 2) {
+					anim.CrossFade (AnimatorName + "IdleToSit", CrossfadeVal);
+				} else if (Pose == 3) {
+					anim.CrossFade (AnimatorName + "IdleToLay", CrossfadeVal);
+				}
+
+				CurrentPose = Pose;
+			}
+			//Dead
+			else if (CurrentPose == 6) {
+				if (Pose == 2) {
+					anim.CrossFade (AnimatorName +"IdleToSit", CrossfadeVal);
+				} else if (Pose == 3) {
+					anim.CrossFade (AnimatorName + "IdleToLay", CrossfadeVal);
+				} 
+
+				CurrentPose = Pose;
+			}
+		}
+	}
+
+	public void FixedUpdate()
+	{
+		fsm.CurrentState.Reason();
+		fsm.CurrentState.Act();
+	}
+
+	// The NPC has two states: FollowPath and ChasePlayer
+	// If it's on the first state and SawPlayer transition is fired, it changes to ChasePlayer
+	// If it's on ChasePlayerState and LostPlayer transition is fired, it returns to FollowPath
+	private void MakeFSM()
+	{
+		StandState stand = new StandState(this);
+		stand.AddTransition(Transition.ToDead, StateID.Dead);
+		stand.AddTransition(Transition.ToWalk, StateID.Walk);
+		stand.AddTransition(Transition.ToRun, StateID.Run);
+		stand.AddTransition(Transition.ToSit, StateID.Sit);
+		stand.AddTransition(Transition.ToJump, StateID.Jump);
+		stand.AddTransition(Transition.ToLay, StateID.Lay);
+
+
+		WalkState walk = new WalkState(this);
+		walk.AddTransition(Transition.ToDead, StateID.Dead);
+		walk.AddTransition(Transition.ToStand, StateID.Stand);
+		walk.AddTransition(Transition.ToRun, StateID.Run);
+		walk.AddTransition(Transition.ToSit, StateID.Sit);
+		walk.AddTransition(Transition.ToJump, StateID.Jump);
+		walk.AddTransition(Transition.ToLay, StateID.Lay);
+
+		RunState run = new RunState(this);
+		run.AddTransition(Transition.ToDead, StateID.Dead);
+		run.AddTransition(Transition.ToStand, StateID.Stand);
+		run.AddTransition(Transition.ToWalk, StateID.Walk);
+		run.AddTransition(Transition.ToSit, StateID.Sit);
+		run.AddTransition(Transition.ToJump, StateID.Jump);
+		run.AddTransition(Transition.ToLay, StateID.Lay);
+
+		SitState sit = new SitState(this);
+		sit.AddTransition(Transition.ToDead, StateID.Dead);
+		sit.AddTransition(Transition.ToStand, StateID.Stand);
+		sit.AddTransition(Transition.ToRun, StateID.Run);
+		sit.AddTransition(Transition.ToWalk, StateID.Walk);
+		sit.AddTransition(Transition.ToJump, StateID.Jump);
+		sit.AddTransition(Transition.ToLay, StateID.Lay);
+
+		LayState lay = new LayState(this);
+		lay.AddTransition(Transition.ToDead, StateID.Dead);
+		lay.AddTransition(Transition.ToStand, StateID.Stand);
+		lay.AddTransition(Transition.ToRun, StateID.Run);
+		lay.AddTransition(Transition.ToSit, StateID.Sit);
+		lay.AddTransition(Transition.ToJump, StateID.Jump);
+		lay.AddTransition(Transition.ToWalk, StateID.Walk);
+
+		JumpState jump = new JumpState(this);
+		jump.AddTransition(Transition.ToDead, StateID.Dead);
+		jump.AddTransition(Transition.ToStand, StateID.Stand);
+		jump.AddTransition(Transition.ToRun, StateID.Run);
+		jump.AddTransition(Transition.ToSit, StateID.Sit);
+		jump.AddTransition(Transition.ToWalk, StateID.Walk);
+		jump.AddTransition(Transition.ToLay, StateID.Lay);
+
+		DeadState dead = new DeadState(this);
+		dead.AddTransition(Transition.ToWalk, StateID.Walk);
+		dead.AddTransition(Transition.ToStand, StateID.Stand);
+		dead.AddTransition(Transition.ToRun, StateID.Run);
+		dead.AddTransition(Transition.ToSit, StateID.Sit);
+		dead.AddTransition(Transition.ToJump, StateID.Jump);
+		dead.AddTransition(Transition.ToLay, StateID.Lay);
+
+		fsm = new FSMSystem();
+		fsm.AddState(stand);
+		fsm.AddState(walk);
+		fsm.AddState(run);
+		fsm.AddState(sit);
+		fsm.AddState(lay);
+		fsm.AddState(jump);
+		fsm.AddState(dead);
+	}
+}
+
+
+
+
+
+
+public class StandState : FSMState
+{
+	DogControl m_dog;
+	public StandState(DogControl npc)
+	{
+		stateID = StateID.Stand;
+		m_dog = npc;
+	}
+
+	public override void DoBeforeEntering() 
+	{ 
+		Debug.Log ("Enter Stand state");
+		Vector3 cameraForward = Camera.main.transform.forward;
+		cameraForward.y = 0;
+		m_dog.transform.forward = -cameraForward;
+
+		m_dog.Move = 0;
+		m_dog.anim.SetFloat ("Move", m_dog.Move);
+		m_dog.anim.CrossFade (m_dog.AnimatorName + "Idle", 0.5f);
+
+		if (m_dog.Pose != 0) {
+			m_dog.ChangePose = true;
+		}
+		m_dog.Pose = 0;
+	}
+
+	public override void Reason()
+	{
+
+	}
+
+	public override void Act()
+	{
+
+	}
+
+	public override void DoBeforeLeaving() 
+	{ 
+
+	}
+
+}
+
+public class JumpState : FSMState
+{
+	DogControl m_dog;
+	public JumpState(DogControl npc)
+	{
+		stateID = StateID.Jump;
+		m_dog = npc;
+	}
+
+	public override void DoBeforeEntering() 
+	{ 
+		Debug.Log ("Enter Jump state");
+
+		m_dog.Move = 0;
+		m_dog.anim.SetFloat ("Move", m_dog.Move);
+		m_dog.anim.CrossFade (m_dog.AnimatorName + "Jump", 0.5f);
+
+		if (m_dog.Pose != 1) {
+			m_dog.ChangePose = true;
+		}
+		m_dog.Pose = 1;
+	}
+
+	public override void Reason()
+	{
+
+	}
+
+	public override void Act()
+	{
+
+	}
+
+	public override void DoBeforeLeaving() 
+	{ 
+		Debug.Log ("Leave Jump state");
+
+		m_dog.Move = 0;
+		m_dog.anim.SetFloat ("Move", m_dog.Move);
+		m_dog.anim.CrossFade (m_dog.AnimatorName + "Idle", 0.5f);
+
+	}
+
+}
+
+public class SitState : FSMState
+{
+	DogControl m_dog;
+	public SitState(DogControl npc)
+	{
+		stateID = StateID.Sit;
+		m_dog = npc;
+	}
+
+	public override void DoBeforeEntering() 
+	{ 
+		Debug.Log ("Enter Sit state");
+
+		m_dog.Move = 0;
+		m_dog.anim.SetFloat ("Move", m_dog.Move);
+		//m_dog.anim.CrossFade (m_dog.AnimatorName + "Sit", 0.5f);
+
+		if (m_dog.Pose != 2) {
+			m_dog.ChangePose = true;
+		}
+		m_dog.Pose = 2;
+	}
+
+	public override void Reason()
+	{
+
+	}
+
+	public override void Act()
+	{
+
+	}
+
+	public override void DoBeforeLeaving() 
+	{ 
+		Debug.Log ("Leave Sit state");
+
+		m_dog.Move = 0;
+		m_dog.anim.SetFloat ("Move", m_dog.Move);
+		m_dog.anim.CrossFade (m_dog.AnimatorName + "Idle", 0.5f);
+
+	}
+}
+
+
+public class LayState : FSMState
+{
+	DogControl m_dog;
+	public LayState(DogControl npc)
+	{
+		stateID = StateID.Lay;
+		m_dog = npc;
+	}
+
+	public override void DoBeforeEntering() 
+	{ 
+		Debug.Log ("Enter Lay state");
+
+		m_dog.Move = 0;
+		m_dog.anim.SetFloat ("Move", m_dog.Move);
+	//	m_dog.anim.CrossFade (m_dog.AnimatorName + "Lay", 0.5f);
+
+		if (m_dog.Pose != 3) {
+			m_dog.ChangePose = true;
+		}
+		m_dog.Pose = 3;
+	}
+
+	public override void Reason()
+	{
+
+	}
+
+	public override void Act()
+	{
+
+	}
+
+	public override void DoBeforeLeaving() 
+	{ 
+		Debug.Log ("Leave Lay state");
+
+		m_dog.Move = 0;
+		m_dog.anim.SetFloat ("Move", m_dog.Move);
+		m_dog.anim.CrossFade (m_dog.AnimatorName + "Idle", 0.5f);
+
+	}
+}
+
+public class WalkState : FSMState
+{
+	DogControl m_dog;
+	Vector3 targetPos = Vector3.zero;
+	Vector3 movingDirection = Vector3.zero;
+
+	public WalkState(DogControl npc) 
+	{ 
+		stateID = StateID.Walk;
+		m_dog = npc;
+
+		float randX = UnityEngine.Random.Range (-1.0f, 1.0f);
+		float randY = UnityEngine.Random.Range (-1.0f, 1.0f);
+		targetPos = new Vector3(randX,0,randY);
+
+		movingDirection = targetPos - m_dog.transform.position;
+		movingDirection = movingDirection.normalized;
+		m_dog.transform.forward = movingDirection;
+	}
+
+	public override void DoBeforeEntering() 
+	{ 
+		Debug.Log ("Enter Walk state");
+		m_dog.Move = 1;
+		m_dog.anim.SetFloat ("Move", m_dog.Move);
+
+		if (m_dog.Pose != 4) {
+			m_dog.ChangePose = true;
+		}
+		m_dog.Pose = 4;
+	}
+
+	public override void Reason()
+	{
+
+	}
+
+	public override void Act()
+	{
+		if (Vector3.Distance (m_dog.transform.position, targetPos) > 0.2f) {
+			m_dog.transform.position += 0.002f * movingDirection;
+		} 
+		else {
+			m_dog.transform.position = targetPos;
+
+			float randX = UnityEngine.Random.Range (-1.0f, 1.0f);
+			float randY = UnityEngine.Random.Range (-1.0f, 1.0f);
+			targetPos = new Vector3(randX,0,randY);
+
+			movingDirection = targetPos - m_dog.transform.position;
+			movingDirection = movingDirection.normalized;
+			m_dog.transform.forward = movingDirection;
+		}
+	}
+
+	public override void DoBeforeLeaving() 
+	{ 
+
+	}
+
+} 
+
+public class RunState : FSMState
+{
+	DogControl m_dog;
+	Vector3 targetPos = Vector3.zero;
+	Vector3 movingDirection = Vector3.zero;
+
+	public RunState(DogControl npc) 
+	{ 
+		stateID = StateID.Run;
+		m_dog = npc;
+
+		float randX = UnityEngine.Random.Range (-1.0f, 1.0f);
+		float randY = UnityEngine.Random.Range (-1.0f, 1.0f);
+		targetPos = new Vector3(randX,0,randY);
+
+		movingDirection = targetPos - m_dog.transform.position;
+		movingDirection = movingDirection.normalized;
+		m_dog.transform.forward = movingDirection;
+
+		if (m_dog.Pose != 5) {
+			m_dog.ChangePose = true;
+		}
+		m_dog.Pose = 5;
+	}
+
+	public override void DoBeforeEntering() 
+	{ 
+		Debug.Log ("Enter Run state");
+		m_dog.Move = 7;
+		m_dog.anim.SetFloat ("Move", m_dog.Move);
+	}
+
+	public override void Reason()
+	{
+
+	}
+
+	public override void Act()
+	{
+		if (Vector3.Distance (m_dog.transform.position, targetPos) > 0.2f) {
+			m_dog.transform.position += 0.004f * movingDirection;
+		} 
+		else {
+			m_dog.transform.position = targetPos;
+
+			float randX = UnityEngine.Random.Range (-1.0f, 1.0f);
+			float randY = UnityEngine.Random.Range (-1.0f, 1.0f);
+			targetPos = new Vector3(randX,0,randY);
+
+			movingDirection = targetPos - m_dog.transform.position;
+			movingDirection = movingDirection.normalized;
+			m_dog.transform.forward = movingDirection;
+		}
+	}
+
+} 
+
+public class DeadState : FSMState
+{
+	DogControl m_dog;
+	public DeadState(DogControl npc)
+	{
+		stateID = StateID.Dead;
+		m_dog = npc;
+	}
+
+	public override void DoBeforeEntering() 
+	{ 
+		Debug.Log ("Enter Lay state");
+
+		m_dog.Move = 0;
+		m_dog.anim.SetFloat ("Move", m_dog.Move);
+		m_dog.anim.CrossFade (m_dog.AnimatorName + "Death", 0.5f);
+
+		if (m_dog.Pose != 6) {
+			m_dog.ChangePose = true;
+		}
+		m_dog.Pose = 6;
+	}
+
+	public override void Reason()
+	{
+
+	}
+
+	public override void Act()
+	{
+
+	}
+
+}
